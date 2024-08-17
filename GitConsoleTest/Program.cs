b8bb0f099f37f9d7046470f18e02cfa75a2437c9
Himanshu <h.shekhar134@gmail.com>
2024-08-17 23:41:45 +0530

    Build failure test

--- GitConsoleTest/Program.cs
+++ GitConsoleTest/Program.cs
@@ -1,12 +1,20 @@
 ﻿namespace GitConsoleTest
 {
-    internal class Program
+    public class Program
     {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello, World!");
-            Console.WriteLine("Hello, Aswinee!");
+         public const string name = "Hello";
 
+        public static void Main(string[] args)
+        {
+            int num1 = 10
+            int num2 = 20;
+            int sum = 0;
+            //Console.WriteLine("Hello, World!");
+            Console.WriteLine($"Sum is : { num2 - num1 }");
+
+            Console.ReadKey();
         }
+
+
     }
 }
